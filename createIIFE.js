f7d957d72e4ff93010c5cd039e37b9fd1ab6b8e7
bahrus <anderson.bruce.b@gmail.com>
2018-11-04 15:44:02 -0500

    add iife

---
+++ createIIFE.js
@@ -0,0 +1,3 @@
+const jiife = require('jiife');
+const xl = 'node_modules/xtal-latx/';
+jiife.processFiles([xl + 'define.js', xl + 'observeCssSelector.js', xl + 'xtal-latx.js', 'css-observe.js'], 'css-observe.iife.js');
---
+++ css-observe.iife.js
@@ -0,0 +1,196 @@
+
+    //@ts-check
+    (function () {
+    function define(custEl) {
+    let tagName = custEl.is;
+    if (customElements.get(tagName)) {
+        console.warn('Already registered ' + tagName);
+        return;
+    }
+    customElements.define(tagName, custEl);
+}
+function observeCssSelector(superClass) {
+    const eventNames = ["animationstart", "MSAnimationStart", "webkitAnimationStart"];
+    return class extends superClass {
+        addCSSListener(id, targetSelector, insertListener) {
+            // See https://davidwalsh.name/detect-node-insertion
+            if (this._boundInsertListener)
+                return;
+            const styleInner = /* css */ `
+            @keyframes ${id} {
+                from {
+                    opacity: 0.99;
+                }
+                to {
+                    opacity: 1;
+                }
+            }
+    
+            ${targetSelector}{
+                animation-duration: 0.001s;
+                animation-name: ${id};
+            }
+            `;
+            const style = document.createElement('style');
+            style.innerHTML = styleInner;
+            const host = getHost(this);
+            if (host !== null) {
+                host.shadowRoot.appendChild(style);
+            }
+            else {
+                document.body.appendChild(style);
+            }
+            this._boundInsertListener = insertListener.bind(this);
+            const container = host ? host.shadowRoot : document;
+            eventNames.forEach(name => {
+                container.addEventListener(name, this._boundInsertListener, false);
+            });
+            // container.addEventListener("animationstart", this._boundInsertListener, false); // standard + firefox
+            // container.addEventListener("MSAnimationStart", this._boundInsertListener, false); // IE
+            // container.addEventListener("webkitAnimationStart", this._boundInsertListener, false); // Chrome + Safari
+        }
+        disconnectedCallback() {
+            if (this._boundInsertListener) {
+                const host = getHost(this);
+                const container = host ? host.shadowRoot : document;
+                eventNames.forEach(name => {
+                    container.removeEventListener(name, this._boundInsertListener);
+                });
+                // document.removeEventListener("animationstart", this._boundInsertListener); // standard + firefox
+                // document.removeEventListener("MSAnimationStart", this._boundInsertListener); // IE
+                // document.removeEventListener("webkitAnimationStart", this._boundInsertListener); // Chrome + Safari
+            }
+            if (super.disconnectedCallback)
+                super.disconnectedCallback();
+        }
+    };
+}
+const disabled = 'disabled';
+function XtallatX(superClass) {
+    return class extends superClass {
+        constructor() {
+            super(...arguments);
+            this._evCount = {};
+        }
+        static get observedAttributes() {
+            return [disabled];
+        }
+        get disabled() {
+            return this._disabled;
+        }
+        set disabled(val) {
+            this.attr(disabled, val, '');
+        }
+        attr(name, val, trueVal) {
+            const v = val ? 'set' : 'remove'; //verb
+            this[v + 'Attribute'](name, trueVal || val);
+        }
+        to$(n) {
+            const mod = n % 2;
+            return (n - mod) / 2 + '-' + mod;
+        }
+        incAttr(name) {
+            const ec = this._evCount;
+            if (name in ec) {
+                ec[name]++;
+            }
+            else {
+                ec[name] = 0;
+            }
+            this.attr('data-' + name, this.to$(ec[name]));
+        }
+        attributeChangedCallback(name, oldVal, newVal) {
+            switch (name) {
+                case disabled:
+                    this._disabled = newVal !== null;
+                    break;
+            }
+        }
+        de(name, detail) {
+            const eventName = name + '-changed';
+            const newEvent = new CustomEvent(eventName, {
+                detail: detail,
+                bubbles: true,
+                composed: false,
+            });
+            this.dispatchEvent(newEvent);
+            this.incAttr(eventName);
+            return newEvent;
+        }
+        _upgradeProperties(props) {
+            props.forEach(prop => {
+                if (this.hasOwnProperty(prop)) {
+                    let value = this[prop];
+                    delete this[prop];
+                    this[prop] = value;
+                }
+            });
+        }
+    };
+}
+const selector = 'selector';
+const observe = 'observe';
+class CssObserve extends observeCssSelector(XtallatX(HTMLElement)) {
+    static get is() { return 'css-observe'; }
+    static get observedAttributes() {
+        return super.observedAttributes.concat([observe, selector]);
+    }
+    connectedCallback() {
+        this._upgradeProperties([selector, observe]);
+        this._connected = true;
+        this.onPropsChange();
+    }
+    get selector() {
+        return this._selector;
+    }
+    set selector(val) {
+        this.attr(selector, val);
+    }
+    get observe() {
+        return this._observe;
+    }
+    set observe(val) {
+        this.attr(observe, val, '');
+    }
+    attributeChangedCallback(name, oldVal, newVal) {
+        super.attributeChangedCallback(name, oldVal, newVal);
+        const fldName = '_' + name;
+        switch (name) {
+            case selector:
+                this[fldName] = newVal;
+                break;
+            case observe:
+                this[fldName] = newVal !== null;
+                break;
+        }
+        this.onPropsChange();
+    }
+    onPropsChange() {
+        if (this._connected && !this.id) {
+            console.warn('id required for ' + this.localName);
+        }
+        if (this._disabled || !this._connected || !this._observe || !this.id)
+            return;
+        this.addCSSListener(this.id, this._selector, this.insertListener);
+    }
+    get latestMatch() {
+        return this._latestMatch;
+    }
+    set latestMatch(val) {
+        this._latestMatch = val;
+        this.de('latest-match', {
+            value: val,
+        });
+    }
+    insertListener(e) {
+        if (e.animationName === this.id) {
+            const target = e.target;
+            setTimeout(() => {
+                this.latestMatch = target;
+            }, 0);
+        }
+    }
+}
+define(CssObserve);
+    })();  
+        
