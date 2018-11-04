0649ed01181bddf1f547d5c15e0b9ff716d4b917
bahrus <anderson.bruce.b@gmail.com>
2018-11-04 15:59:35 -0500

    add getHost ref

--- createIIFE.js
+++ createIIFE.js
@@ -1,3 +1,3 @@
 const jiife = require('jiife');
 const xl = 'node_modules/xtal-latx/';
-jiife.processFiles([xl + 'define.js', xl + 'observeCssSelector.js', xl + 'xtal-latx.js', 'css-observe.js'], 'css-observe.iife.js');
+jiife.processFiles([xl + 'define.js', xl + 'getHost.js', xl + 'observeCssSelector.js', xl + 'xtal-latx.js', 'css-observe.js'], 'css-observe.iife.js');
